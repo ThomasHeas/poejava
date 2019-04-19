5134bd576fe371ca5147af5fefe4958cbbc6b351
Souheil <souheil.sultan@gmail.com>
2019-04-19 14:58:29 +0200

    tp0

---
+++ src/fr/imie/cours/poe/tp0/MainTP0.java
@@ -0,0 +1,11 @@
+package fr.imie.cours.poe.tp0;
+
+public class MainTP0 {
+
+    public static void main(String[] args) {
+        // les appels statiques seront de la forme Utilitaire.maFonction();
+        // NomClasse.nomFonction();
+        Utilitaire.direToto();
+    }
+
+}
---
+++ src/fr/imie/cours/poe/tp0/Utilitaire.java
@@ -0,0 +1,10 @@
+package fr.imie.cours.poe.tp0;
+
+public class Utilitaire {
+
+    // visibilité static typeDeRetour nomFonction() {}
+    public static void direToto() {
+        System.out.println("Toto");
+    }
+
+}
